2760b611e4d9b94d050e6686fcc18ee38ab903dc
ftilde <ftilde@tamepointer.de>
2024-01-27 19:24:10 +0100

    Fix button event detection in short duration selects

--- src/drivers/button.rs
+++ src/drivers/button.rs
@@ -35,12 +35,29 @@ impl Button {
         self.pin.get_level()
     }
 
+    fn set_last_state(&mut self, state: Level) {
+        let now = Instant::now();
+        match state {
+            PRESSED => self.last_press = now,
+            RELEASED => self.last_release = now,
+        }
+        self.last_state.0 = state;
+        self.last_state.1 = now;
+    }
+
     pub async fn wait_for_state(&mut self, l: Level) {
-        let in_debounce = self.last_state.1.elapsed() < DEBOUNCE_TIME;
+        let mut in_debounce = self.last_state.1.elapsed() < DEBOUNCE_TIME;
         let debounced_state = if in_debounce {
             self.last_state.0
         } else {
-            self.state()
+            let current_state = self.state();
+
+            if self.last_state.0 != current_state {
+                self.set_last_state(current_state);
+                in_debounce = true;
+            }
+
+            current_state
         };
 
         if debounced_state != l {
@@ -49,13 +66,12 @@ impl Button {
             }
             self.pin.wait_for_any_edge().await;
 
-            let now = Instant::now();
-            match l {
-                PRESSED => self.last_press = now,
-                RELEASED => self.last_release = now,
-            }
-            self.last_state.0 = l;
-            self.last_state.1 = now
+            // Previously, the state was no l, but we detected an edge, so the state must have been
+            // in l either before or now. Either way, we waited for l, and we assume the last
+            // measured state was l as well. If it isn't, then it will be detected on the next
+            // call.
+
+            self.set_last_state(l);
         }
     }
 
@@ -68,7 +84,9 @@ impl Button {
     }
 
     pub async fn wait_for_press(&mut self) -> Duration {
-        self.wait_for_down().await;
+        if self.last_state.0 != PRESSED {
+            self.wait_for_down().await;
+        }
         self.wait_for_up().await;
         self.last_press.elapsed()
     }
