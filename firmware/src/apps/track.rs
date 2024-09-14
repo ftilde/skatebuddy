b7a1d944121eec6aa322cb127656be2cfdafc46a
ftilde <ftilde@tamepointer.de>
2024-09-14 22:28:16 +0200

    Add quick and dirty track recording

--- firmware/src/apps/track.rs
+++ firmware/src/apps/track.rs
@@ -1,17 +1,21 @@
+use arrform::*;
+use bytemuck::Zeroable;
 use core::fmt::Write;
-use drivers::gps::CasicMsg;
+use drivers::gps::{CasicMsg, NavPv};
 use drivers::lpm013m1126c::Rgb111;
+use drivers::time::{Duration, Instant};
 use drivers::{futures::select, gps::CasicMsgConfig};
+use embedded_graphics::geometry::{Point, Size};
 use embedded_graphics::mono_font::MonoTextStyle;
+use littlefs2::path::PathBuf;
 
+use crate::ui::ButtonStyle;
 use crate::{render_top_bar, ui::TextWriter, Context};
 
 pub async fn track(ctx: &mut Context) {
     let font = &embedded_graphics::mono_font::ascii::FONT_8X13;
     //let sl = TextStyle::new(font, embedded_graphics::pixelcolor::BinaryColor::On);
     let sl = MonoTextStyle::new(font, embedded_graphics::pixelcolor::BinaryColor::On);
-
-    //let mut touch = ctx.touch.enabled(&mut ctx.twi0).await;
 
     let mut gps = drivers::gps::GPSReceiver::new(CasicMsgConfig {
         nav_pv: 1,
@@ -47,6 +51,33 @@ pub async fn track(ctx: &mut Context) {
         vel_north: 0,
     };
 
+    let mut touch = ctx.touch.enabled(&mut ctx.twi0).await;
+
+    let button_style = ButtonStyle {
+        fill: Rgb111::blue(),
+        highlight: Rgb111::white(),
+        font,
+    };
+
+    let mut record_button = crate::ui::Button::from(crate::ui::ButtonDefinition {
+        position: Point::new(0, 126),
+        size: Size::new(50, 50),
+        style: &button_style,
+        text: "Record",
+    });
+
+    enum RecordingState {
+        Idle,
+        Recording {
+            since: Instant,
+            path: PathBuf,
+            samples: [NavPv; 30],
+            sample: usize,
+        },
+    }
+
+    let mut recording_state = RecordingState::Idle;
+
     loop {
         ctx.lcd.fill(Rgb111::black());
         render_top_bar(&mut ctx.lcd, &ctx.battery).await;
@@ -63,10 +94,20 @@ pub async fn track(ctx: &mut Context) {
         let _ = writeln!(w, "vel_east: {:?}", state.vel_east);
         let _ = writeln!(w, "vel_north: {:?}", state.vel_north);
 
+        if matches!(recording_state, RecordingState::Idle) {
+            record_button.render(&mut *ctx.lcd).unwrap();
+        }
+
         ctx.lcd.present().await;
 
-        match select::select(gps.receive(), ctx.button.wait_for_press()).await {
-            select::Either::First(msg) => match msg {
+        match select::select3(
+            gps.receive(),
+            ctx.button.wait_for_press(),
+            touch.wait_for_action(),
+        )
+        .await
+        {
+            select::Either3::First(msg) => match msg {
                 CasicMsg::NavPv(s) => {
                     crate::println!("pv msg: {:?}", s);
                     state.num_satellites = s.num_sv;
@@ -77,6 +118,38 @@ pub async fn track(ctx: &mut Context) {
                     state.height = s.height_m;
                     state.vel_east = s.east_velocity_m_s;
                     state.vel_north = s.north_velocity_m_s;
+
+                    if let RecordingState::Recording {
+                        since,
+                        path,
+                        sample,
+                        samples,
+                    } = &mut recording_state
+                    {
+                        samples[*sample] = s;
+                        *sample += 1;
+
+                        if since.elapsed() > Duration::from_secs(1000) || *sample == samples.len() {
+                            ctx.flash
+                                .with_fs(|fs| {
+                                    fs.open_file_with_options_and_then(
+                                        |o| o.write(true).create(true).append(true),
+                                        &path,
+                                        |file| {
+                                            use littlefs2::io::Write;
+                                            for i in 0..*sample {
+                                                let sample_val = samples[i];
+                                                file.write_all(bytemuck::bytes_of(&sample_val))?;
+                                            }
+                                            Ok(())
+                                        },
+                                    )
+                                })
+                                .await
+                                .unwrap();
+                            recording_state = RecordingState::Idle;
+                        }
+                    }
                 }
                 CasicMsg::NavGpsInfo(i) => {
                     crate::println!("gps msg: {:?}", i);
@@ -85,8 +158,34 @@ pub async fn track(ctx: &mut Context) {
                 }
                 _ => {}
             },
-            select::Either::Second(_) => {
+            select::Either3::Second(_) => {
                 break;
+            }
+            select::Either3::Third(e) => {
+                ctx.backlight.active().await;
+                if record_button.clicked(&e) {
+                    let path = ctx
+                        .flash
+                        .with_fs(|fs| {
+                            fs.create_dir_all(b"/gps/\0".try_into().unwrap())?;
+                            for i in 0.. {
+                                let path =
+                                    PathBuf::from(arrform!(40, "/gps/samples{}.bin", i).as_str());
+                                if fs.metadata(&path) == Err(littlefs2::io::Error::NoSuchEntry) {
+                                    return Ok(path);
+                                }
+                            }
+                            panic!("Too many recordings");
+                        })
+                        .await
+                        .unwrap();
+                    recording_state = RecordingState::Recording {
+                        since: Instant::now(),
+                        path,
+                        samples: [NavPv::zeroed(); 30],
+                        sample: 0,
+                    }
+                }
             }
         }
     }
