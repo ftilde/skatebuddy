0b5fe20db1cd7c1bcd45af30060aa2e15342cd75
ftilde <ftilde@tamepointer.de>
2024-09-15 21:51:03 +0200

    Add (experimental) kalman filter for pos/velocity

--- tools/analyze_gps/src/main.rs
+++ tools/analyze_gps/src/main.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 
-use drivers_shared::gps::NavPv;
+use drivers_shared::gps::NavigationData;
 use plotpy::{Curve, Plot};
-use util::gps::LonLat;
+use util::gps::{KalmanFilter, LonLat};
 
-fn plot_values(vals: &[(f32, f32)]) -> Result<(), Box<dyn Error>> {
+fn plot_values(vals: &[(f32, f32)], equal: bool) -> Result<(), Box<dyn Error>> {
     let mut curve = Curve::new();
     curve.set_line_width(2.0);
 
@@ -16,7 +16,7 @@ fn plot_values(vals: &[(f32, f32)]) -> Result<(), Box<dyn Error>> {
 
     let mut plot = Plot::new();
     plot.add(&curve)
-        .set_equal_axes(true)
+        .set_equal_axes(equal)
         .grid_and_labels("x", "y");
 
     if let Err(e) = plot.show("out.svg") {
@@ -25,36 +25,90 @@ fn plot_values(vals: &[(f32, f32)]) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+fn plot_values_multiple(vals: &[&[(f32, f32)]], equal: bool) -> Result<(), Box<dyn Error>> {
+    let mut plot = Plot::new();
+    for (i, vals) in vals.iter().enumerate() {
+        let mut curve = Curve::new();
+        curve.set_line_width(2.0);
 
-fn read_file(name: &str) -> Vec<NavPv> {
+        curve.points_begin();
+        for (x, y) in *vals {
+            curve.points_add(x, y);
+        }
+        curve.points_end();
+        curve.set_label(&i.to_string());
+
+        plot.add(&curve);
+    }
+
+    if let Err(e) = plot
+        .legend()
+        .grid_and_labels("x", "y")
+        .set_equal_axes(equal)
+        .show("out.svg")
+    {
+        println!("{}", e);
+    }
+
+    Ok(())
+}
+
+fn read_file(name: &str) -> Vec<NavigationData> {
     let file = std::fs::File::open(name).unwrap();
 
     let file = unsafe { memmap::Mmap::map(&file).unwrap() };
 
-    let entries: &[drivers_shared::gps::NavPv] = bytemuck::cast_slice(&*file);
+    let entries: &[drivers_shared::gps::NavigationData] = bytemuck::cast_slice(&*file);
 
     entries.to_vec()
+}
+
+fn diag(x: f32, y: f32) -> f32 {
+    (x * x + y * y).sqrt()
 }
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
 
     let files = args[1..].iter().map(|n| read_file(n));
-    let entries: Vec<NavPv> = files.flatten().collect();
+    let entries: Vec<NavigationData> = files.flatten().collect();
 
-    let mut values = Vec::new();
+    let mut positions = Vec::new();
+    let mut positions_filtered = Vec::new();
+
+    let mut acc_x = 0.0;
+    let mut acc_y = 0.0;
+    let mut acc_positions = Vec::new();
+    let mut speeds = Vec::new();
+    let mut speeds_filtered = Vec::new();
     let converter = util::gps::RefConverter::new(LonLat {
         lon: entries[0].longitude,
         lat: entries[0].latitude,
     });
-    for pv in entries {
-        let p = converter.to_relative(LonLat {
-            lon: pv.longitude,
-            lat: pv.latitude,
-        });
-        values.push((p.east as f32, p.north as f32));
-        println!("PV: {:?}", pv);
+
+    let mut kalman_filter = KalmanFilter::new();
+    for pv in &entries {
+        let p = converter.to_relative_full(pv);
+        positions.push((p.pos_east as f32, p.pos_north as f32));
+        acc_x += pv.east_velocity_m_s;
+        acc_y += pv.north_velocity_m_s;
+        acc_positions.push((acc_x as f32, acc_y as f32));
+        let ground_speed = diag(pv.north_velocity_m_s, pv.east_velocity_m_s);
+        speeds.push((pv.run_time as f32 / 1000.0, ground_speed));
+
+        let filtered = kalman_filter.add_value(p);
+        positions_filtered.push((filtered.pos_east as f32, filtered.pos_north as f32));
+
+        let ground_speed = diag(filtered.vel_north, filtered.vel_east);
+        speeds_filtered.push((pv.run_time as f32 / 1000.0, ground_speed));
     }
 
-    plot_values(values.as_slice()).unwrap();
+    plot_values_multiple(
+        &[positions.as_slice(), positions_filtered.as_slice()],
+        false,
+    )
+    .unwrap();
+    plot_values_multiple(&[speeds.as_slice(), speeds_filtered.as_slice()], false).unwrap();
+    plot_values(acc_positions.as_slice(), true).unwrap();
+    plot_values(positions.as_slice(), true).unwrap();
 }
--- util/src/gps.rs
+++ util/src/gps.rs
@@ -1,4 +1,5 @@
-use nalgebra::{Rotation3, Vector3};
+use drivers_shared::gps::NavigationData;
+use nalgebra::{Matrix2, Matrix4, Rotation3, Vector2, Vector3, Vector4};
 
 const EARTH_RADIUS: f64 = 6378137.0;
 const CENTER_VEC: Vector3<f64> = Vector3::new(EARTH_RADIUS, 0.0, 0.0);
@@ -40,6 +41,131 @@ impl RefConverter {
         RelativePos {
             east: relative_3d[1],
             north: relative_3d[2],
+        }
+    }
+
+    pub fn to_relative_full(&self, p: &NavigationData) -> RelativeNavigationData {
+        let rel_pos = self.to_relative(LonLat {
+            lon: p.longitude,
+            lat: p.latitude,
+        });
+
+        RelativeNavigationData {
+            run_time: p.run_time,
+            height_anomaly: p.height_anomaly,
+            pos_north: rel_pos.north as f32,
+            pos_east: rel_pos.east as f32,
+            horizontal_variance: p.horizontal_variance,
+            vertical_variance: p.vertical_variance,
+            north_velocity_m_s: p.north_velocity_m_s,
+            east_velocity_m_s: p.east_velocity_m_s,
+            variance_speed_2d: p.variance_speed_2d,
+            heavenly_velocity_m_s: p.heavenly_velocity_m_s,
+        }
+    }
+}
+
+pub struct RelativeNavigationData {
+    pub run_time: u32,
+    pub height_anomaly: f32,
+    pub pos_north: f32,
+    pub pos_east: f32,
+    pub horizontal_variance: f32,
+    pub vertical_variance: f32,
+    pub north_velocity_m_s: f32,
+    pub east_velocity_m_s: f32,
+    pub variance_speed_2d: f32,
+    pub heavenly_velocity_m_s: f32,
+}
+
+pub struct KalmanFilter {
+    state_a_priori: Vector4<f32>,
+    p_a_priori: Matrix4<f32>,
+    t_in_s: f32,
+}
+
+fn square(v: f32) -> f32 {
+    v * v
+}
+
+pub struct State {
+    pub pos_east: f32,
+    pub pos_north: f32,
+    pub vel_east: f32,
+    pub vel_north: f32,
+}
+
+impl KalmanFilter {
+    pub fn new() -> Self {
+        Self {
+            state_a_priori: Vector4::new(0.0, 0.0, 0.0, 0.0),
+            p_a_priori: Matrix4::identity() * 100.0,
+            t_in_s: 0.0,
+        }
+    }
+
+    #[allow(non_snake_case)]
+    pub fn add_value(&mut self, measurement: RelativeNavigationData) -> State {
+        let avg_g = 0.1;
+        let avg_a = avg_g * 9.81;
+
+        let nt = measurement.run_time as f32 / 1000.0;
+        //let dt = nt - self.t_in_s;
+        let dt = 1.0; //TODO: something is broken here when we take the true time values. Probably
+                      //because of initial value?
+        assert!(dt > 0.0);
+        assert!(dt < 2.0);
+        self.t_in_s = nt;
+
+        let G = Vector2::new(0.5 * dt * dt, dt);
+        //G = np.array([dt, dt, 0.5*dt*dt, 0.5*dt*dt])
+        let Q2 = Matrix2::from_fn(|r, c| G[r] * G[c]) * square(avg_a * dt);
+        let mut Q = Matrix4::zeros();
+        for d in [0, 1] {
+            for i in [0, 1] {
+                for j in [0, 1] {
+                    Q[(2 * i + d, 2 * j + d)] = Q2[(i, j)];
+                }
+            }
+        }
+
+        let mut F = Matrix4::identity();
+        F[(0, 2)] = dt;
+        F[(1, 3)] = dt;
+
+        let R_k_diag = Vector4::new(
+            measurement.horizontal_variance,
+            measurement.horizontal_variance,
+            measurement.variance_speed_2d,
+            measurement.variance_speed_2d,
+        );
+
+        let R_k = Matrix4::from_diagonal(&R_k_diag);
+
+        let observation = Vector4::new(
+            measurement.pos_east,
+            measurement.pos_north,
+            measurement.east_velocity_m_s,
+            measurement.north_velocity_m_s,
+        );
+
+        //Correction
+        let y_k = observation - self.state_a_priori;
+        let S_k = self.p_a_priori + R_k;
+        let K_k = self.p_a_priori * S_k.try_inverse().unwrap();
+
+        let state = self.state_a_priori + K_k * y_k;
+        let P = (Matrix4::identity() - K_k) * self.p_a_priori;
+
+        //prediction
+        self.state_a_priori = F * state;
+        self.p_a_priori = (F * P * F.transpose()) + Q;
+
+        State {
+            pos_east: state[0],
+            pos_north: state[1],
+            vel_east: state[2],
+            vel_north: state[3],
         }
     }
 }
