5d74b19fbedbed6aa41a5686757b4b33e4052ad5
ftilde <ftilde@tamepointer.de>
2024-09-15 21:59:17 +0200

    Use proper values for dt in kalmanfilter

--- util/src/gps.rs
+++ util/src/gps.rs
@@ -78,10 +78,14 @@ pub struct RelativeNavigationData {
     pub heavenly_velocity_m_s: f32,
 }
 
-pub struct KalmanFilter {
+struct KalmanFilterState {
     state_a_priori: Vector4<f32>,
     p_a_priori: Matrix4<f32>,
     t_in_s: f32,
+}
+
+pub struct KalmanFilter {
+    state: Option<KalmanFilterState>,
 }
 
 fn square(v: f32) -> f32 {
@@ -97,75 +101,96 @@ pub struct State {
 
 impl KalmanFilter {
     pub fn new() -> Self {
-        Self {
-            state_a_priori: Vector4::new(0.0, 0.0, 0.0, 0.0),
-            p_a_priori: Matrix4::identity() * 100.0,
-            t_in_s: 0.0,
-        }
+        Self { state: None }
     }
 
     #[allow(non_snake_case)]
     pub fn add_value(&mut self, measurement: RelativeNavigationData) -> State {
-        let avg_g = 0.1;
-        let avg_a = avg_g * 9.81;
+        if let Some(kalman_state) = &mut self.state {
+            let avg_g = 0.1;
+            let avg_a = avg_g * 9.81;
 
-        let nt = measurement.run_time as f32 / 1000.0;
-        //let dt = nt - self.t_in_s;
-        let dt = 1.0; //TODO: something is broken here when we take the true time values. Probably
-                      //because of initial value?
-        assert!(dt > 0.0);
-        assert!(dt < 2.0);
-        self.t_in_s = nt;
+            let nt = measurement.run_time as f32 / 1000.0;
+            let dt = nt - kalman_state.t_in_s;
+            assert!(dt > 0.0);
+            kalman_state.t_in_s = nt;
 
-        let G = Vector2::new(0.5 * dt * dt, dt);
-        //G = np.array([dt, dt, 0.5*dt*dt, 0.5*dt*dt])
-        let Q2 = Matrix2::from_fn(|r, c| G[r] * G[c]) * square(avg_a * dt);
-        let mut Q = Matrix4::zeros();
-        for d in [0, 1] {
-            for i in [0, 1] {
-                for j in [0, 1] {
-                    Q[(2 * i + d, 2 * j + d)] = Q2[(i, j)];
+            let G = Vector2::new(0.5 * dt * dt, dt);
+            //G = np.array([dt, dt, 0.5*dt*dt, 0.5*dt*dt])
+            let Q2 = Matrix2::from_fn(|r, c| G[r] * G[c]) * square(avg_a * dt);
+            let mut Q = Matrix4::zeros();
+            for d in [0, 1] {
+                for i in [0, 1] {
+                    for j in [0, 1] {
+                        Q[(2 * i + d, 2 * j + d)] = Q2[(i, j)];
+                    }
                 }
             }
-        }
 
-        let mut F = Matrix4::identity();
-        F[(0, 2)] = dt;
-        F[(1, 3)] = dt;
+            let mut F = Matrix4::identity();
+            F[(0, 2)] = dt;
+            F[(1, 3)] = dt;
 
-        let R_k_diag = Vector4::new(
-            measurement.horizontal_variance,
-            measurement.horizontal_variance,
-            measurement.variance_speed_2d,
-            measurement.variance_speed_2d,
-        );
+            let R_k_diag = Vector4::new(
+                measurement.horizontal_variance,
+                measurement.horizontal_variance,
+                measurement.variance_speed_2d,
+                measurement.variance_speed_2d,
+            );
 
-        let R_k = Matrix4::from_diagonal(&R_k_diag);
+            let R_k = Matrix4::from_diagonal(&R_k_diag);
 
-        let observation = Vector4::new(
-            measurement.pos_east,
-            measurement.pos_north,
-            measurement.east_velocity_m_s,
-            measurement.north_velocity_m_s,
-        );
+            let observation = Vector4::new(
+                measurement.pos_east,
+                measurement.pos_north,
+                measurement.east_velocity_m_s,
+                measurement.north_velocity_m_s,
+            );
 
-        //Correction
-        let y_k = observation - self.state_a_priori;
-        let S_k = self.p_a_priori + R_k;
-        let K_k = self.p_a_priori * S_k.try_inverse().unwrap();
+            //Correction
+            let y_k = observation - kalman_state.state_a_priori;
+            let S_k = kalman_state.p_a_priori + R_k;
+            let K_k = kalman_state.p_a_priori * S_k.try_inverse().unwrap();
 
-        let state = self.state_a_priori + K_k * y_k;
-        let P = (Matrix4::identity() - K_k) * self.p_a_priori;
+            let state = kalman_state.state_a_priori + K_k * y_k;
+            let P = (Matrix4::identity() - K_k) * kalman_state.p_a_priori;
 
-        //prediction
-        self.state_a_priori = F * state;
-        self.p_a_priori = (F * P * F.transpose()) + Q;
+            //prediction
+            kalman_state.state_a_priori = F * state;
+            kalman_state.p_a_priori = (F * P * F.transpose()) + Q;
 
-        State {
-            pos_east: state[0],
-            pos_north: state[1],
-            vel_east: state[2],
-            vel_north: state[3],
+            State {
+                pos_east: state[0],
+                pos_north: state[1],
+                vel_east: state[2],
+                vel_north: state[3],
+            }
+        } else {
+            let variance_diag = Vector4::new(
+                measurement.horizontal_variance,
+                measurement.horizontal_variance,
+                measurement.variance_speed_2d,
+                measurement.variance_speed_2d,
+            );
+
+            let state = Vector4::new(
+                measurement.pos_east,
+                measurement.pos_north,
+                measurement.east_velocity_m_s,
+                measurement.north_velocity_m_s,
+            );
+
+            self.state = Some(KalmanFilterState {
+                state_a_priori: state,
+                p_a_priori: Matrix4::from_diagonal(&variance_diag),
+                t_in_s: measurement.run_time as f32 / 1000.0,
+            });
+            State {
+                pos_east: state[0],
+                pos_north: state[1],
+                vel_east: state[2],
+                vel_north: state[3],
+            }
         }
     }
 }
