a7466e50c35eefa250558d2dbe35e52affe6863c
ftilde <ftilde@tamepointer.de>
2024-09-18 21:25:48 +0200

    Replay old gps data for drivers-simu

--- drivers-simu/src/gps.rs
+++ drivers-simu/src/gps.rs
@@ -22,6 +22,8 @@ pub struct GPSReceiver<'a> {
     config: CasicMsgConfig,
     initialized: Instant,
     send_queue: VecDeque<CasicMsg>,
+    replay_naviation_data: Vec<NavigationData>,
+    replay_naviation_data_index: usize,
 }
 
 impl Drop for GPSReceiver<'_> {
@@ -30,13 +32,36 @@ impl Drop for GPSReceiver<'_> {
     }
 }
 
+fn try_read_replay_data() -> Result<Vec<NavigationData>, &'static str> {
+    let file_name =
+        std::env::var("REPLAY_NAVIGATION_DATA").map_err(|_| "No env var REPLAY_NAVIGATION_DATA")?;
+
+    let file = std::fs::File::open(file_name).map_err(|_| "Failed to open navigation data file")?;
+
+    let file = unsafe { memmap::Mmap::map(&file).unwrap() };
+
+    let entries: &[drivers_shared::gps::NavigationData] = bytemuck::cast_slice(&*file);
+
+    Ok(entries.to_vec())
+}
+
 impl<'a> GPSReceiver<'a> {
     pub async fn new(config: CasicMsgConfig) -> GPSReceiver<'a> {
+        let replay_naviation_data = match try_read_replay_data() {
+            Ok(d) => d,
+            Err(e) => {
+                eprint!("Error reading replay data: {}", e);
+                Vec::new()
+            }
+        };
+
         GPSReceiver {
             config,
             _marker: Default::default(),
             initialized: Instant::now(),
             send_queue: Default::default(),
+            replay_naviation_data,
+            replay_naviation_data_index: 0,
         }
     }
 
@@ -61,32 +86,65 @@ impl<'a> GPSReceiver<'a> {
             let sat_in_fix = sec.saturating_sub(3).min(7) as u8;
             let run_time = sec as u32 * 1000;
             if time_to_send(self.config.nav_pv) {
-                self.send_queue.push_back(CasicMsg::NavPv(NavPv {
-                    run_time,
-                    pos_valid: (sat_in_fix > 0) as u8 * 7,
-                    vel_valid: (sat_in_fix > 0) as u8 * 7,
-                    system: 1,
-                    num_sv: sat_in_fix,
-                    num_sv_gps: sat_in_fix,
-                    num_sv_bds: 0,
-                    num_sv_gln: 0,
-                    _reserved: 0,
-                    location_dop: 1.0,
-                    longitude: sec as f64 / 1000.0,
-                    latitude: sec as f64 / 1000.0,
-                    height_m: 0.0,
-                    height_anomaly: 0.0,
-                    horizontal_variance: 1.0,
-                    vertical_variance: 1.0,
-                    north_velocity_m_s: 1.0,
-                    east_velocity_m_s: 1.0,
-                    heavenly_velocity_m_s: 0.0,
-                    speed_3d: 0.0,
-                    speed_2d: 0.0,
-                    heading: 0.0,
-                    variance_speed_2d: 1.0,
-                    variance_heading: 1.0,
-                }));
+                if let Some(d) = self
+                    .replay_naviation_data
+                    .get(self.replay_naviation_data_index)
+                {
+                    self.replay_naviation_data_index += 1;
+                    self.send_queue.push_back(CasicMsg::NavPv(NavPv {
+                        run_time,
+                        pos_valid: (sat_in_fix > 0) as u8 * 7,
+                        vel_valid: (sat_in_fix > 0) as u8 * 7,
+                        system: 1,
+                        num_sv: sat_in_fix,
+                        num_sv_gps: sat_in_fix,
+                        num_sv_bds: 0,
+                        num_sv_gln: 0,
+                        _reserved: 0,
+                        location_dop: 1.0,
+                        longitude: d.longitude,
+                        latitude: d.latitude,
+                        height_m: 0.0,
+                        height_anomaly: d.height_anomaly,
+                        horizontal_variance: d.horizontal_variance,
+                        vertical_variance: d.vertical_variance,
+                        north_velocity_m_s: d.north_velocity_m_s,
+                        east_velocity_m_s: d.north_velocity_m_s,
+                        heavenly_velocity_m_s: d.heavenly_velocity_m_s,
+                        speed_3d: 0.0,
+                        speed_2d: 0.0,
+                        heading: 0.0,
+                        variance_speed_2d: 1.0,
+                        variance_heading: 1.0,
+                    }));
+                } else {
+                    self.send_queue.push_back(CasicMsg::NavPv(NavPv {
+                        run_time,
+                        pos_valid: (sat_in_fix > 0) as u8 * 7,
+                        vel_valid: (sat_in_fix > 0) as u8 * 7,
+                        system: 1,
+                        num_sv: sat_in_fix,
+                        num_sv_gps: sat_in_fix,
+                        num_sv_bds: 0,
+                        num_sv_gln: 0,
+                        _reserved: 0,
+                        location_dop: 1.0,
+                        longitude: sec as f64 / 1000.0,
+                        latitude: sec as f64 / 1000.0,
+                        height_m: 0.0,
+                        height_anomaly: 0.0,
+                        horizontal_variance: 1.0,
+                        vertical_variance: 1.0,
+                        north_velocity_m_s: 1.0,
+                        east_velocity_m_s: 1.0,
+                        heavenly_velocity_m_s: 0.0,
+                        speed_3d: 0.0,
+                        speed_2d: 0.0,
+                        heading: 0.0,
+                        variance_speed_2d: 1.0,
+                        variance_heading: 1.0,
+                    }));
+                }
             }
             if time_to_send(self.config.nav_time) {
                 self.send_queue.push_back(CasicMsg::NavTimeUTC(NavTimeUTC {
