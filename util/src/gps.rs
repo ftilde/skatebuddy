84eda6addb5902bfec718aa5d01ed70b27cd59f3
ftilde <ftilde@tamepointer.de>
2024-09-21 18:54:31 +0200

    Reset kalman filter if encountering non-invertible matrix

--- util/src/gps.rs
+++ util/src/gps.rs
@@ -143,6 +143,14 @@ pub struct State {
     pub vel: nalgebra::Vector2<f32>,
 }
 
+//fn positive_semidefinite(mat: &Matrix4<f32>) -> bool {
+//    // Using https://en.wikipedia.org/wiki/Sylvester's_criterion
+//    mat.fixed_view::<1, 1>(0, 0).determinant() >= 0.0
+//        && mat.fixed_view::<2, 2>(0, 0).determinant() >= 0.0
+//        && mat.fixed_view::<3, 3>(0, 0).determinant() >= 0.0
+//        && mat.determinant() >= 0.0
+//}
+
 impl KalmanFilter {
     pub fn new() -> Self {
         Self { state: None }
@@ -161,7 +169,7 @@ impl KalmanFilter {
 
             let G = Vector2::new(0.5 * dt * dt, dt);
             //G = np.array([dt, dt, 0.5*dt*dt, 0.5*dt*dt])
-            let Q2 = Matrix2::from_fn(|r, c| G[r] * G[c]) * square(avg_a * dt);
+            let Q2 = Matrix2::from_fn(|r, c| G[r] * G[c]) * square(avg_a);
             let mut Q = Matrix4::zeros();
             for d in [0, 1] {
                 for i in [0, 1] {
@@ -182,6 +190,9 @@ impl KalmanFilter {
                 measurement.variance_speed_2d,
             );
 
+            assert!(measurement.horizontal_variance > 0.0);
+            assert!(measurement.variance_speed_2d > 0.0);
+
             let R_k = Matrix4::from_diagonal(&R_k_diag);
 
             let observation = Vector4::new(
@@ -194,43 +205,67 @@ impl KalmanFilter {
             //Correction
             let y_k = observation - kalman_state.state_a_priori;
             let S_k = kalman_state.p_a_priori + R_k;
-            let K_k = kalman_state.p_a_priori * S_k.try_inverse().unwrap();
-
+            let Some(S_k_inv) = S_k.try_inverse() else {
+                return self.init_state(measurement);
+            };
+            let K_k = kalman_state.p_a_priori * S_k_inv;
             let state = kalman_state.state_a_priori + K_k * y_k;
             let P = (Matrix4::identity() - K_k) * kalman_state.p_a_priori;
 
             //prediction
             kalman_state.state_a_priori = F * state;
             kalman_state.p_a_priori = (F * P * F.transpose()) + Q;
+            //assert!(
+            //    positive_semidefinite(&kalman_state.p_a_priori),
+            //    "apriori: {:#?}\nQ: {:#?}\nP: {:#?}\n F: {:#?}\nK_k {:#?}",
+            //    kalman_state.p_a_priori,
+            //    Q,
+            //    P,
+            //    F,
+            //    K_k,
+            //);
+            //assert!(
+            //    kalman_state.p_a_priori.is_invertible(),
+            //    "apriori: {:#?}\nQ: {:#?}\nP: {:#?}\n F: {:#?}\nK_k {:#?}",
+            //    kalman_state.p_a_priori,
+            //    Q,
+            //    P,
+            //    F,
+            //    K_k,
+            //);
 
             State {
                 pos: Vector2::new(state[0], state[1]),
                 vel: Vector2::new(state[2], state[3]),
             }
         } else {
-            let variance_diag = Vector4::new(
-                measurement.horizontal_variance,
-                measurement.horizontal_variance,
-                measurement.variance_speed_2d,
-                measurement.variance_speed_2d,
-            );
+            self.init_state(measurement)
+        }
+    }
 
-            let state = Vector4::new(
-                measurement.pos[0],
-                measurement.pos[1],
-                measurement.vel[0],
-                measurement.vel[1],
-            );
+    pub fn init_state(&mut self, measurement: RelativeNavigationData) -> State {
+        let variance_diag = Vector4::new(
+            measurement.horizontal_variance,
+            measurement.horizontal_variance,
+            measurement.variance_speed_2d,
+            measurement.variance_speed_2d,
+        );
 
-            self.state = Some(KalmanFilterState {
-                state_a_priori: state,
-                p_a_priori: Matrix4::from_diagonal(&variance_diag),
-                t_in_s: measurement.run_time as f32 / 1000.0,
-            });
-            State {
-                pos: Vector2::new(state[0], state[1]),
-                vel: Vector2::new(state[2], state[3]),
-            }
+        let state = Vector4::new(
+            measurement.pos[0],
+            measurement.pos[1],
+            measurement.vel[0],
+            measurement.vel[1],
+        );
+
+        self.state = Some(KalmanFilterState {
+            state_a_priori: state,
+            p_a_priori: Matrix4::from_diagonal(&variance_diag),
+            t_in_s: measurement.run_time as f32 / 1000.0,
+        });
+        State {
+            pos: Vector2::new(state[0], state[1]),
+            vel: Vector2::new(state[2], state[3]),
         }
     }
 }
